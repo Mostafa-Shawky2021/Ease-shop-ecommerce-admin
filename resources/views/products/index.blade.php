025b3c181bc2644e38cbe99955e298e4c0f6b8d8
MostafaShawky <mostafashawky20177@gmail.com>
2023-08-23 00:35:10 +0300

    README file

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,36 +1,35 @@
 @extends('layout.app')
 
 @section('header-content')
-<h5>المنتجات</h5>
-<a class="btn-add" href="{{ route('products.create') }}">
-    اضافة منتج
-    <i class="icon fa fa-plus"></i>
-</a>
+    <h5>المنتجات</h5>
+    <a class="btn-add" href="{{ route('products.create') }}">
+        اضافة منتج
+        <i class="icon fa fa-plus"></i>
+    </a>
 @endsection
 
 @section('content')
-@php
-// to render datatable with required data
-$productQuery = Request::query('status');
-$dataTableId = $productQuery === 'trashed' ? 'trashedProductsWrapper' : 'productsWrapper';
-@endphp
-
-@include('partials.formmessage')
-
-<div class="datatable-wrapper" id="{{ $dataTableId }}">
     @php
-    $datatableHeaderOptions = ['withSearch' => true];
-    $datatableHeaderOptions = $productQuery === 'trashed' ? array_merge($datatableHeaderOptions, ['withRestoreBtn' => true]) :
-    $datatableHeaderOptions;
-
+        // to render datatable with required data
+        $productQuery = Request::query('status');
+        $dataTableId = $productQuery === 'trashed' ? 'trashedProductsWrapper' : 'productsWrapper';
     @endphp
-    @include('partials.datatableheader', $datatableHeaderOptions)
-    <div class="table-responsive">
-        {{ $dataTable->table(['class' => 'table table-data-layout']) }}
+
+    @include('partials.formmessage')
+
+    <div class="datatable-wrapper" id="{{ $dataTableId }}">
+        @php
+            $datatableHeaderOptions = ['withSearch' => true];
+            $datatableHeaderOptions = $productQuery === 'trashed' ? array_merge($datatableHeaderOptions, ['withRestoreBtn' => true]) : $datatableHeaderOptions;
+            
+        @endphp
+        @include('partials.datatableheader', $datatableHeaderOptions)
+        <div class="table-responsive">
+            {{ $dataTable->table(['class' => 'table table-data-layout']) }}
+        </div>
     </div>
-</div>
 @endsection
 
 @push('scripts')
-{{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
 @endpush
